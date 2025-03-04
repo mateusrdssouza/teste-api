bb05051f31548b4ce91ea7232bf893eb598cb63a
Mateus Rodrigues de Souza <mateusrdssouza@gmail.com>
2025-03-04 00:31:06 -0300

    feat: company controller test

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,10 @@
 use App\Http\Controllers\Api\CompanyController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('companies', CompanyController::class);
+Route::apiResource('companies', CompanyController::class)->only([
+    'index',
+    'store',
+    'show',
+    'update',
+    'destroy'
+]);
---
+++ tests/Feature/Api/CompanyControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CompanyControllerTest extends TestCase
+{
+    use RefreshDatabase;
+}
